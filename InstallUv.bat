158396fe259a5594c9bab401a4c8316516a13620
TimothyAllman <166746823+TimothyAllman@users.noreply.github.com>
2025-03-05 23:22:10 +0200

    Create InstallUv.bat

---
+++ InstallUv.bat
@@ -0,0 +1,8 @@
+:: this is a .bat file comment
+::
+::
+:: we use uv for package managment and dependency resolution 
+:: as of Mar 2025 we can install it using the the below .bat command/comand line 
+:: see https://docs.astral.sh/uv/getting-started/installation/ for more info
+
+winget install --id=astral-sh.uv  -e
