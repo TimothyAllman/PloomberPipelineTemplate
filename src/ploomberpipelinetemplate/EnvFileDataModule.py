2ecd553d69c999777b8a7e4c3c736c69742603eb
TimothyAllman <166746823+TimothyAllman@users.noreply.github.com>
2025-03-22 18:44:25 +0200

    remember to self

--- src/ploomberpipelinetemplate/EnvFileDataModule.py
+++ src/ploomberpipelinetemplate/EnvFileDataModule.py
@@ -1,8 +1,9 @@
 class EnvFileData:
     def __init__(
+        self,
         env_thing_1: str = "mything",
         env_thing_2: int = 5,
     ):
-        ENV_THING_1 = env_thing_1
-        ENV_THING_2 = env_thing_2
-        MY_FOLDER = f"{ENV_THING_1}_{ENV_THING_2}"
+        self.ENV_THING_1 = env_thing_1
+        self.ENV_THING_2 = env_thing_2
+        self.MY_FOLDER = f"{self.ENV_THING_1}_{self.ENV_THING_2}_folder"
