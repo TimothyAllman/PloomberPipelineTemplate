7ba89e230d59957ae711926cebb069020aed0d9b
TimothyAllman <166746823+TimothyAllman@users.noreply.github.com>
2025-05-17 23:49:43 +0200

    remove commandline stuff

---
+++ Script_PloomberPlot.py
@@ -0,0 +1,16 @@
+from ploomberpipelinetemplate.EnvFileDataModule import EnvFileData
+from ploomberpipelinetemplate.PipelineFunctionsModule import CreatePipeline
+
+
+myEnvDictionary = EnvFileData(
+    env_thing_1="this_thing",
+    env_thing_2="prod",
+)
+
+dag = CreatePipeline(
+    envParams=myEnvDictionary,
+)
+
+dag.plot(
+    backend="mermaid",
+)
