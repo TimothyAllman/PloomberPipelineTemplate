1f69218dab94df8a751f05fedf9ad62861c6dcd2
TimothyAllman <166746823+TimothyAllman@users.noreply.github.com>
2025-05-17 22:42:18 +0200

    add excel folder

--- src/ploomberpipelinetemplate/ConstantsForFilePathsDataModule.py
+++ src/ploomberpipelinetemplate/ConstantsForFilePathsDataModule.py
@@ -1,7 +1,8 @@
 # top level folder paths
+_OUTPUT_FOLDER_NAME = "output"  # we will save all output here
 _SHARED_VARIABLES_FOLDER_NAME = "sharedVariables"  # we intermidiate variables here
 _NOTEBOOKS_FOLDER_NAME = "executedTasks"  # exectuted tasks that are traansformed into ipynb files will go here
-_OUTPUT_FOLDER_NAME = "output"  # we will save all output here
+_EXTRA_EXCEL_FOLDER_NAME = "excelFiles"  # we will save all output here
 
 
 #
@@ -13,6 +14,7 @@ _OUTPUT_FOLDER_NAME = "output"  # we will save all output here
 #
 #
 # file paths to excel files
+_MY_OUTPUT_XSLX_FILE_PATH = f"{_OUTPUT_FOLDER_NAME}/{_EXTRA_EXCEL_FOLDER_NAME}/myOutput.xlsx"  # we will save all output here
 
 
 #
