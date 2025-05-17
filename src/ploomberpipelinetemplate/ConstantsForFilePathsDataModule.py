cfbd3cd010527dbebc53384885334aba81ac5199
TimothyAllman <166746823+TimothyAllman@users.noreply.github.com>
2025-05-17 22:24:20 +0200

    move constants files paths to src module

---
+++ src/ploomberpipelinetemplate/ConstantsForFilePathsDataModule.py
@@ -0,0 +1,37 @@
+# top level folder paths
+_SHARED_VARIABLES_FOLDER_NAME = "sharedVariables"  # we intermidiate variables here
+_NOTEBOOKS_FOLDER_NAME = "executedTasks"  # exectuted tasks that are traansformed into ipynb files will go here
+_OUTPUT_FOLDER_NAME = "output"  # we will save all output here
+
+
+#
+#
+#
+#
+#
+#
+#
+#
+# file paths to excel files
+
+
+#
+#
+#
+#
+#
+#
+#
+#
+# file paths to images
+
+
+#
+#
+#
+#
+#
+#
+#
+#
+# file paths to other files
