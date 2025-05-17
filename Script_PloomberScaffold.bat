f235fa14fe826e507a3111f3f62fa4994587141e
TimothyAllman <166746823+TimothyAllman@users.noreply.github.com>
2025-05-17 22:08:04 +0200

    use scripts instead of notebooks

---
+++ Script_PloomberScaffold.bat
@@ -0,0 +1,11 @@
+:: this is a .bat file comment
+:: and this is a small .bat script to invoke the xyz function in the cmd
+:: running this file will:
+:: 1. 
+:: 2.
+:: 3.
+:: NB IMPORTNANT NOTE!! 
+:: see https://docs.astral.sh/uv/getting-started/installation/ for more info
+
+cd myPipeline
+ploomber scaffold
--- Script_UvSync.bat
+++ Script_UvSync.bat
@@ -1,5 +1,5 @@
 :: this is a .bat file comment
-::
+:: and thsis is a 
 ::
 :: in order to set the prjoect up/get all he correct packages/dependencies installed in the right places
 :: run the below command
